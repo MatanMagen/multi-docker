259ec38d4a62695a2cce746e0d35805d3566c3db
matan magen <matanmagen2002@gmail.com>
2023-03-28 14:48:36 +0300

    change client again

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            first project AWS
+            first project AWS - SGL 
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
